69a2f84c97d7669b9d4cb8838b786706208032ec
Yogesh1216 <ynain1998@gmail.com>
2025-08-14 01:00:58 +0530

    properties

---
+++ 6_Properties/LearningProperties.cs
@@ -0,0 +1,37 @@
+﻿namespace Properties
+{
+    class Person
+    {
+        /*
+        private DateTime _birthdate;
+        public DateTime Birthdate
+        {
+            get { return _birthdate; }
+            set { _birthdate = value; }
+        }
+        */
+
+        // if there is no logic we can also write it as   
+        //  internally it creates a private field     | // requirement - set the birthdate only once , we cannot modify it. so create it private set.
+
+        public string Name { get; set; }
+        public DateTime Birthdate { get; private set; }  // it means its readonly if its private se we cant assign the value after constructor.
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+        public int Age
+        {
+            get
+            {
+                var timespan = DateTime.Today - Birthdate;
+                var years = timespan.Days / 365;
+                return years;
+            }
+            // age should be calculated on the basis of birthdate so no need to set the age.
+        }
+
+        
+    }
+}
---
+++ 6_Properties/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Properties
+{
+    /// <summary>
+    /// 1. Properties: Encapsulates a getter and setter for a field.
+    /// </summary>
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person1 = new Person(new DateTime(1990, 1, 1));
+            // person1.Birthdate = new DateTime(2000, 1, 1); // This will give an error because Birthdate has a private setter.
+            Console.WriteLine($"Person 1 Age: {person1.Age}");
+        }
+    }
+}
