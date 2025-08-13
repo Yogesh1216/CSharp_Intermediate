37d321070a067116433be996997fcdb3dd2a5c05
Yogesh1216 <ynain1998@gmail.com>
2025-08-13 23:29:33 +0530

    Encapsulation

---
+++ 5_AccessModifiers/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace LearningAccessModifiers
+{
+    public class Person
+    {
+        private DateTime _birthdate;
+        public void SetBirthdate(DateTime birthdate)
+        {
+            if (birthdate > DateTime.Now)
+            {
+                throw new ArgumentException("Birthdate cannot be in the future.");
+            }
+            _birthdate = birthdate;
+        }
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+}
---
+++ 5_AccessModifiers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LearningAccessModifiers
+{
+    /// <summary>
+    /// 1. Access Modifiers: public, private, protected, internal
+    /// 2. Encapsulation: not accessing the fields directly, using methods to set and get values
+    /// </summary>
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1990, 1, 1));
+            Console.WriteLine(person.GetBirthdate());
+
+        }
+    }
+}
